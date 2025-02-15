039f0102c81c5be9030be1c60a4e0e3be411400d
neshadx <neshadimagify@gmail.com>
2025-02-15 23:55:56 +0600

    done

---
+++ t2.js
@@ -0,0 +1,15 @@
+/*2. Add or remove elements
+1. Declare an array of 3 tourist destinations
+2. Add a new tourist destination to your tourist array
+3. Add two more to your array
+4. Remove the last tourist destination you have added
+5. display the final array as output
+*/
+
+let tourist = ["Tsc", "Bandarban", "Dhanmondi"];
+
+tourist.push("cox");
+tourist.push("mirpur");
+tourist.push("Uttara");
+tourist.pop();
+console.log(tourist);
