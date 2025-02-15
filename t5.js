5bb5922d6dd47649a9d1c3b3fcd2dad360b0baa7
neshadx <neshadimagify@gmail.com>
2025-02-15 23:58:44 +0600

    done

---
+++ t5.js
@@ -0,0 +1,22 @@
+/*
+5. Combining Arrays
+
+**Instructions:**
+
+1. Create two arrays of your choice.
+2. Use the concat method to combine the two arrays into a new array.
+3. Print both the original arrays and the combined array using console.log().
+*/
+
+let pc = ["Windows", "MacBook", "Linux"];
+let mobile = ["Iphone", "Samsung", "Vivo"];
+let pcMobile = pc.concat(mobile);
+
+// console.log(pc.concat(mobile));
+console.log(pc);
+console.log(mobile);
+console.log(pcMobile);
+
+// console.log("PC Array:", pc);
+// console.log("Mobile Array:", mobile);
+// console.log("Combined PC and Mobile Array:", pcMobile);
