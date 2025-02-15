c5b1547cb745da7a5c1a2c4c342f9711d45cd978
neshadx <neshadimagify@gmail.com>
2025-02-15 23:54:28 +0600

    done

---
+++ t1.js
@@ -0,0 +1,12 @@
+/**
+ ### 1. Declare an array 
+1. Declare an array with 5 elements containing fruits
+2. console log the 3rd index element
+3. change the value of the 2nd index element to jambura
+4. console log the final array
+ */
+
+let fruits = ['apple', 'orange', 'mango', 'berry', 'banana'];
+console.log(fruits.indexOf[3]);
+fruits [2] = "jambura";
+console.log(fruits);
